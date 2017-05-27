7510706e072488f4ab810f279bfdb6db6187e7d4
George Mylonas <mylonasg88@gmail.com>
2017-05-27 14:42:51 +0100

    Removed unnecessary comments.

--- vhosts/index.php
+++ vhosts/index.php
@@ -1,14 +1,2 @@
 <?php
 phpinfo();
-//$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-//
-//$get  = "GET: " . json_encode($_GET) . "\n";
-//$post = "POST: " . json_encode($_POST) . "\n";
-//
-//fwrite($myfile, $get);
-//fwrite($myfile, $post);
-//fclose($myfile);
-//
-//var_dump($_SERVER['HTTP_COOKIE']);
-//echo implode(" ", $_GET);
-//echo 'here';
